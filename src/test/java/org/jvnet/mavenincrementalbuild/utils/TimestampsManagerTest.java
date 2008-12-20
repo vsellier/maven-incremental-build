f761cc89ae01b788246d6939b2df097741c17220
Vincent Sellier <vincent.sellier@gmail.com>
2008-12-20 14:59:59 +0000

    Removed unused imports and code formatting
    git-svn-id: https://svn.java.net/svn/maven-incremental-build~svn/trunk@71 197ba5b8-c926-ea00-ecaf-fae12fcbe926

--- src/test/java/org/jvnet/mavenincrementalbuild/utils/TimestampsManagerTest.java
+++ src/test/java/org/jvnet/mavenincrementalbuild/utils/TimestampsManagerTest.java
@@ -1,8 +1,6 @@
 package org.jvnet.mavenincrementalbuild.utils;
 
-import java.io.IOException;
-
-import junit.framework.*;
+import junit.framework.TestCase;
 
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugin.logging.SystemStreamLog;
@@ -11,23 +9,26 @@ public class TimestampsManagerTest extends TestCase {
 	private final static String OUTPUT_DIRECTORY = "target";
 
 	private Log logger;
-	
+
 	public void setUp() {
 		logger = new SystemStreamLog();
 	}
-	
+
 	public void testSerializationAndDeserialization() throws Exception {
-		TimestampsManager manager = new TimestampsManager(logger, OUTPUT_DIRECTORY);
-		
+		TimestampsManager manager = new TimestampsManager(logger,
+				OUTPUT_DIRECTORY);
+
 		manager.loadPreviousTimestamps();
 		manager.setTimestamp("test", new Long(10));
 		manager.setTimestamp("test2", new Long(100));
-		
+
 		manager.saveTimestamps();
-		
+
 		manager = new TimestampsManager(logger, OUTPUT_DIRECTORY);
 		manager.loadPreviousTimestamps();
-		assertEquals("Wrong timestamp", new Long(10), manager.getTimestamp("test"));
-		assertEquals("Wrong timestamp", new Long(100), manager.getTimestamp("test2"));		
+		assertEquals("Wrong timestamp", new Long(10), manager
+				.getTimestamp("test"));
+		assertEquals("Wrong timestamp", new Long(100), manager
+				.getTimestamp("test2"));
 	}
 }
