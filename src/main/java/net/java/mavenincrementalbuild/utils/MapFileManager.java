73c904f5cf32e01cc882615906981893b00db2b9
Vincent Sellier <vincent.sellier@gmail.com>
2011-03-21 23:40:20 +0000

    migration packages and groupId from org.jvnet to net.java
    git-svn-id: https://svn.java.net/svn/maven-incremental-build~svn/trunk@113 197ba5b8-c926-ea00-ecaf-fae12fcbe926

---
+++ src/main/java/net/java/mavenincrementalbuild/utils/MapFileManager.java
@@ -0,0 +1,37 @@
+package net.java.mavenincrementalbuild.utils;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.maven.plugin.logging.Log;
+
+/**
+ * Managed timestamps file.
+ * 
+ * @author Vincent Sellier
+ * 
+ */
+public class MapFileManager<K, V> extends FilePersistence<Map<K, V>> {
+
+	/**
+	 * Create the MapFile manager.
+	 * 
+	 * @param directory
+	 *            the dir where the timestamps file will be stored
+	 * @throws IOException
+	 */
+	public MapFileManager(Log logger, String directory, String fileName) throws IOException {
+        super(logger, directory, fileName);
+        data = new HashMap<K, V>();
+	}
+
+
+	public V get(K key) {
+		return data.get(key);
+	}
+
+	public void set(K key, V value) {
+		data.put(key, value);
+	}
+}
